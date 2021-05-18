fb9d397c5c119b7baa8973ca7aa7238cfccc8fb8
CB-JackLi <44012063+CB-JackLi@users.noreply.github.com>
2021-05-18 14:41:37 +0800

    typo fix

--- locale/fr.js
+++ locale/fr.js
@@ -4,7 +4,7 @@
 
 ;(function (global, factory) {
    typeof exports === 'object' && typeof module !== 'undefined'
-       && typeof require === 'function' ? factory(require('../moment,min')) :
+       && typeof require === 'function' ? factory(require('../moment.min')) :
    typeof define === 'function' && define.amd ? define(['../moment.min'], factory) :
    factory(global.moment)
 }(this, (function (moment) { 'use strict';
